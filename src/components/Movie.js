688635e5f752807bf41a878f9b3aa2f30755e2f1
Irin Song <minnpicc@gmail.com>
2024-03-16 15:08:38 +0900

    add styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,25 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres, rating }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img className={styles.img} src={coverImg} alt={title} />
+      <div className={styles.detail}>
+        <h2 className={styles.title}>
+          <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>
+            {title} ({year})
+          </Link>
+        </h2>
+        <p>{summary.length > 250 ? `${summary.slice(0, 250)}...` : summary}</p>
+        <ul className={styles.genres}>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+        <span className={styles.rating}>⭐️ {rating}</span>
+      </div>
     </div>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const [movie, setMovie] = useState([]);
@@ -7,19 +8,25 @@ function Detail() {
   const getMovie = async () => {
     const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
     setMovie(json.data.movie);
-    console.log(movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
   return (
     <div>
-      <h1>{movie.title}</h1>
-      <img src={movie.large_cover_image} />
-      <div>
-        <small>{movie.year}</small>
-        <small>{movie.rating}</small>
-        <p>{movie.description_full}</p>
+      <nav className={styles.nav}>
+        <span>
+          <Link to={`${process.env.PUBLIC_URL}/`}>Home</Link>
+        </span>
+      </nav>
+      <div className={styles.container}>
+        <h1>{movie.title}</h1>
+        <img className={styles.img} src={movie.large_cover_image} alt={movie.title} />
+        <div className={styles.detail}>
+          <span>{movie.year}</span>
+          <span>{movie.rating}</span>
+          <p>{movie.description_full}</p>
+        </div>
       </div>
     </div>
   );
